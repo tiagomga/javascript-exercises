19f5e032b8e3f42e88f521be7097fb2d2b2a88b7
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-08 01:59:53 +0000

    Add solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let continuousString = string
+                            .replaceAll(', ', '')
+                            .replaceAll(' ', '')
+                            .replaceAll('.', '')
+                            .replaceAll('!', '')
+                            .toLowerCase();
+    let stringLength = continuousString.length;
+    for (let i = 0; i < (stringLength / 2); i++) {
+        if (continuousString.charAt(i) !== continuousString.charAt(stringLength-1-i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
