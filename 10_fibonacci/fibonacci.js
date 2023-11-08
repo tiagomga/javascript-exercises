d2622e0e7abdffccf11e0b25c805e97ddfa00513
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-08 02:07:00 +0000

    Add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return fibonacci(num-2) + fibonacci(num-1);
+    }
 };
 
 // Do not edit below this line
